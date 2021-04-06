ebbbabc8de066634659be6e21601709eeb1585c3
inept <63522725+theineptdev@users.noreply.github.com>
2021-04-06 09:29:45 -0500

    vue-notion example demo

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,15 +6,8 @@
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -33,6 +26,7 @@ html {
 
 .button--green {
   display: inline-block;
+  margin: 0.25em;
   border-radius: 4px;
   border: 1px solid #3b8070;
   color: #3b8070;
@@ -47,6 +41,7 @@ html {
 
 .button--grey {
   display: inline-block;
+  margin: 0.25em;
   border-radius: 4px;
   border: 1px solid #35495e;
   color: #35495e;
@@ -58,5 +53,52 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  max-width: 90%;
+  padding: 4em 0.5em;
+  display: block;
+  text-align: center;
+}
+
+.logo {
+  max-width: 100%;
+  padding: 0.5em;
+}
+
+.nav a {
+  color: #333 !important;
+}
+
+.links {
+  padding: 1em 0 1.5em 0;
+}
+
+.posts {
+  margin: 1em;
+  text-align: left;
+}
+
+.container h2 {
+  margin-top: 1.5em;
+}
+
+.container ul {
+  margin: 1em auto;
+  list-style: none;
+}
+
+.container p {
+  margin: 0.75em 0;
+}
+
+.container a {
+  max-width: 75%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,74 +1,62 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        garden-v2
-      </h1>
-      <h2 class="subtitle">
-        Welcome to the View UI + Nuxt.js template
-      </h2>
+    <div class="content">
+      <img
+        src="https://github.com/janniks/vue-notion/raw/main/docs/img/vue-notion.png"
+        alt="vue-notion"
+        width="272px"
+        class="logo"
+      />
+      <p>An unofficial Notion renderer</p>
+
+      <h3 class="nav">
+        <a target="_blank" href="https://github.com/janniks/vue-notion#features"
+          >Features</a
+        >
+        <span> · </span>
+        <a target="_blank" href="https://github.com/janniks/vue-notion#install"
+          >Install</a
+        >
+        <span> · </span>
+        <a
+          target="_blank"
+          href="https://github.com/janniks/vue-notion#basic-example"
+          >Example</a
+        >
+        <span> · </span>
+        <a target="_blank" href="https://github.com/janniks/vue-notion#docs"
+          >Docs</a
+        >
+        <span> · </span>
+        <a target="_blank" href="https://github.com/janniks/vue-notion#credits"
+          >Credits</a
+        >
+      </h3>
+
+      <h2>Example from README</h2>
       <div class="links">
-        <Button type="primary" target="_blank" rel="noopener noreferrer" to="https://nuxtjs.org/">
-          Documentation
-        </Button>
-        <Button target="_blank" rel="noopener noreferrer" to="https://github.com/nuxt/nuxt.js">
-          GitHub
-        </Button>
-        <Button target="_blank" rel="noopener noreferrer" to="https://www.iviewui.com/">
-          View UI
-        </Button>
+        <NuxtLink class="button--green" to="/nuxt">Basic Example</NuxtLink>
+      </div>
+
+      <h2>Static Generated Examples</h2>
+      <div class="links">
+        <p>Blog-style Examples (use Notion as a CMS for Nuxt)</p>
+        <div class="links">
+          <NuxtLink class="button--green" to="/posts">All Blog Posts</NuxtLink>
+          <NuxtLink class="button--green" to="/example">
+            Post via slug <i>(e.g. /example)</i>
+          </NuxtLink>
+        </div>
+      </div>
+      <h2>Example Notion Playground Pages</h2>
+      <div class="links">
+        <NuxtLink class="button--grey" to="/2e22de6b770e4166be301490f6ffd420">
+          react-notion tester
+        </NuxtLink>
+        <NuxtLink class="button--grey" to="/067dd719a912471ea9a3ac10710e7fdf">
+          react-notion-x tester
+        </NuxtLink>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
