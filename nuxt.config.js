a46de2b28c7d2437d1b3df52b51b208af8a7e559
inept <63522725+theineptdev@users.noreply.github.com>
2021-04-06 09:13:11 -0500

    install vue-notion

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    "vue-notion/nuxt",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
