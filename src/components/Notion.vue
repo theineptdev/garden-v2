0d4f7eaa772a8758c3878ffc78e235f1e62facab
inept <63522725+inept-dev@users.noreply.github.com>
2021-04-05 20:27:38 -0500

    integrating vue-notion

---
+++ src/components/Notion.vue
@@ -0,0 +1,20 @@
+<template>
+	<NotionRenderer :blockMap="blockMap" fullPage />
+</template>
+
+<script>
+import { NotionRenderer, getPageBlocks } from 'vue-notion';
+
+export default {
+	components: { NotionRenderer },
+	data: () => ({ blockMap: null }),
+	async created() {
+		// get Notion blocks from the API via a Notion pageId
+		this.blockMap = await getPageBlocks('8c1ab01960b049f6a282dda64a94afc7');
+	},
+};
+</script>
+
+<style>
+@import '../styles/styles.css'; /* optional Notion-like styles */
+</style>
